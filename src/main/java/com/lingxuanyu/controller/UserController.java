b938a06b7b244249bfa59b30937ac9eae70ca8dd
LYF <1451717366@qq.com>
2019-03-13 20:02:46 +0800

    @LYF 配置微信token调用

--- src/main/java/com/lingxuanyu/controller/UserController.java
+++ src/main/java/com/lingxuanyu/controller/UserController.java
@@ -2,25 +2,21 @@ package com.lingxuanyu.controller;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lingxuanyu.entity.User;
-import com.lingxuanyu.service.UserService;
 
 @EnableAutoConfiguration
 @RestController
 public class UserController {
-	
-	@Autowired
-	public UserService userService;
-	
 	@RequestMapping("/getAllUser")
 	public User getUser(Map map) {
-		User user = userService.getUser();
+		User user = new User();
+		user.setId(1);
+		user.setUsername("liyufei");
+		user.setPassword("liyufei");
 		return user;
 	}
 }
